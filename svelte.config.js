0707e95e011d5e8761a04f64ca05a576c71a83a1
Jonas Sontheim <js@qlax.de>
2022-05-03 22:37:37 +0200

    fix faulty import, update config to access src via $root from anywhere

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import path from 'path';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
@@ -9,7 +8,7 @@ const config = {
 		vite: {
 			resolve: {
 				alias: {
-					$root: path.resolve('./src')
+					$root: path.resolve('src')
 				}
 			}
 		}
